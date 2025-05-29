10c8163f9e08f7ecf3acfbf125b495ed5db89c85
BollineniRohith123 <rohithbollineni@gmail.com>
2025-05-29 23:22:16 +0530

    Initial commit: QORE Backend and Frontend

--- talent-spark-frontend/src/pages/roles/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as RolesPage } from './RolesPage';
-export { default as AddRolePage } from './AddRolePage';
-export { default as RoleDetailsPage } from './RoleDetailsPage';
-export { default as RoleEditPage } from './RoleEditPage';
-export { default as RolePermissionsPage } from './RolePermissionsPage';
-export { default as PermissionGroupsPage } from './PermissionGroupsPage';
